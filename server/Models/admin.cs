debe5d2d093fe89e8de739eb74e86637f69585f9
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-10-05 05:04:11 +0530

    feat: user management

---
+++ server/Models/admin.cs
@@ -0,0 +1,34 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace server.Models;
+
+public class Admin
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("username")]
+    public string Username { get; set; }
+
+    [BsonElement("password")]
+    public string Password { get; set; }
+
+    [BsonElement("email")]
+    public string Email { get; set; }
+
+    [BsonElement("adminsCreated")]
+    public List<Admin> AdminsCreated { get; set; } = new List<Admin>();
+
+    [BsonElement("CSRsCreated")]
+    public List<CSR> CSRCreated { get; set; } = new List<CSR>();
+
+    [BsonElement("CustomersApproved")]
+    public List<Users> CustomerApproved { get; set; } = new List<Users>();
+
+    [BsonElement("VendorCreated")]
+    public List<Vendor> VendorCreated { get; set; } = new List<Vendor>();
+
+}
