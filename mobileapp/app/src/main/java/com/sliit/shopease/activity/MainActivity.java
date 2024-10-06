75e66b480e12b35352ec6470671da60fe8a52796
Priyantha <IT21021534@my.sliit.lk>
2024-10-06 22:00:20 +0530

    main page completed

--- mobileapp/app/src/main/java/com/sliit/shopease/activity/MainActivity.java
+++ mobileapp/app/src/main/java/com/sliit/shopease/activity/MainActivity.java
@@ -21,14 +21,20 @@ import com.bumptech.glide.Glide;
 import com.sliit.shopease.Config;
 import com.sliit.shopease.R;
 import com.sliit.shopease.models.Category;
+import com.sliit.shopease.models.Product;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
   RecyclerView rv_categories;
+  RecyclerView rv_products;
   ArrayList<Category> categoryData;
-  LinearLayoutManager linearLayoutManager;
+  ArrayList<Product> productData;
+  LinearLayoutManager h_linearLayoutManager;
+  LinearLayoutManager v_linearLayoutManager;
   RvCategoriesAdapter rvCategoriesAdapter;
+  RvProductsAdapter rvProductsAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     });
 
     rv_categories = findViewById(R.id.rv_categories);
-
+    rv_products = findViewById(R.id.rv_items);
 
     categoryData = new ArrayList<>();
     categoryData.add(new Category("1", "Category 1", "https://picsum.photos/200/300"));
@@ -55,10 +61,27 @@ public class MainActivity extends AppCompatActivity {
     categoryData.add(new Category("6", "Category 6", "https://picsum.photos/240/300"));
     categoryData.add(new Category("7", "Category 7", "https://picsum.photos/250/300"));
 
-    linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+    productData = new ArrayList<>();
+    productData.add(new Product("1", "Product 1", "random description", "https://picsum.photos/300/300", 1000.00));
+    productData.add(new Product("2", "Product 2", "random description", "https://picsum.photos/400/300", 200));
+    productData.add(new Product("3", "Product 3", "random description", "https://picsum.photos/500/300", 150));
+    productData.add(new Product("4", "Product 4", "random description", "https://picsum.photos/600/300", 1000));
+    productData.add(new Product("5", "Product 5", "random description", "https://picsum.photos/700/300", 10));
+    productData.add(new Product("6", "Product 6", "random description", "https://picsum.photos/800/300", 500));
+    productData.add(new Product("7", "Product 7", "random description", "https://picsum.photos/900/300", 560));
+    productData.add(new Product("8", "Product 8", "random description", "https://picsum.photos/000/300", 9000));
+    productData.add(new Product("9", "Product 9", "random description", "https://picsum.photos/250/300", 50));
+    productData.add(new Product("0", "Product 10", "random description", "https://picsum.photos/250/300", 300));
+
+    h_linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
     rvCategoriesAdapter = new RvCategoriesAdapter(categoryData);
-    rv_categories.setLayoutManager(linearLayoutManager);
+    rv_categories.setLayoutManager(h_linearLayoutManager);
     rv_categories.setAdapter(rvCategoriesAdapter);
+
+    v_linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
+    rvProductsAdapter = new RvProductsAdapter(productData);
+    rv_products.setLayoutManager(v_linearLayoutManager);
+    rv_products.setAdapter(rvProductsAdapter);
   }
 
   private void checkUser() {
@@ -91,10 +114,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBindViewHolder(@NonNull RvCategoryHolder holder, int position) {
       holder.txt_categoryName.setText(data.get(position).getName());
-      Glide
-          .with(MainActivity.this)
-          .load(data.get(position).getImageUrl())
-          .into(holder.img_categoryImage);
+      Glide.with(MainActivity.this).load(data.get(position).getImageUrl()).into(holder.img_categoryImage);
     }
 
     class RvCategoryHolder extends RecyclerView.ViewHolder {
@@ -108,6 +128,54 @@ public class MainActivity extends AppCompatActivity {
         img_categoryImage = itemView.findViewById(R.id.sq_img_item);
       }
     }
+  }
+
+  public class RvProductsAdapter extends RecyclerView.Adapter<RvProductsAdapter.RvProductHolder> {
+    private final ArrayList<Product> data;
+
+    public RvProductsAdapter(ArrayList<Product> data) {
+      this.data = data;
+      Collections.shuffle(this.data);
+    }
+
+    @NonNull
+    @Override
+    public RvProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_item_rectangle, parent, false);
+      return new RvProductsAdapter.RvProductHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RvProductHolder holder, int position) {
+      holder.txt_productName.setText(data.get(position).getName());
+      holder.txt_productDescription.setText(data.get(position).getDescription());
+      holder.txt_price.setText(data.get(position).getPriceString());
+
+      Glide.with(MainActivity.this).load(data.get(position).getImageUrl()).into(holder.img_productImage);
+    }
+
+    @Override
+    public int getItemCount() {
+      return data.size();
+    }
+
+    class RvProductHolder extends RecyclerView.ViewHolder {
+      private final TextView txt_productName;
+      private final TextView txt_productDescription;
+      private final ImageView img_productImage;
+      private final TextView txt_price;
+
+      public RvProductHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txt_productName = itemView.findViewById(R.id.rec_txt_item_label);
+        txt_productDescription = itemView.findViewById(R.id.rec_txt_item_description);
+        img_productImage = itemView.findViewById(R.id.rec_img_item);
+        txt_price = itemView.findViewById(R.id.rec_txt_item_price);
+      }
+    }
+
+
   }
 
 }
